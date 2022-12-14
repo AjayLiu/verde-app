17990426c07a66dadeb4fce1b3dc2541fe1d0058
AjayLiu <ajayliu2928@gmail.com>
2022-12-14 01:16:36 -0700

    migrated from expo, cant get FontAwesome to work

---
+++ babel.config.js
@@ -0,0 +1,34 @@
+/* eslint-disable */
+module.exports = function (api) {
+	api.cache(true);
+	const plugins = [
+		[
+			"module-resolver",
+			{
+				root: ["./src/"],
+				extensions: [
+					".ios.js",
+					".android.js",
+					".js",
+					".ts",
+					".tsx",
+					".json",
+				],
+				alias: {
+					tests: ["./tests/"],
+					"@assets": "./src/assets/",
+					"@components": "./src/components",
+					"@config": "./src/config",
+					"@navigation": "./src/navigation",
+					"@screens": "./src/screens",
+					"@utils": "./src/utils",
+					"@hooks": "./src/utils/hooks",
+				},
+			},
+		],
+	];
+	return {
+		presets: ["module:metro-react-native-babel-preset"],
+		plugins,
+	};
+};
